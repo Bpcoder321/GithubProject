2fb52a0e65e3911c77c21769e2439aac283e5693
manu <manu@gmail.com>
2019-09-27 10:28:04 +0530

    second commit

--- Example.java
+++ Example.java
@@ -1,8 +1,10 @@
 class Example{
  public static void main (String[] args){
+	 static int a =199;
       wishUser();
 }
   public void wishUser(){
    System.out.println("hye..good morning");
+   
 }
 }
