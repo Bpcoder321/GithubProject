2ff19be4b99089f314958bb31e03b95f522dc033
Bpcoder321 <50881830+Bpcoder321@users.noreply.github.com>
2019-09-27 10:25:40 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,8 +1,12 @@
 class Example{
  public static void main (String[] args){
       wishUser();
+     greetUser()
 }
   public void wishUser(){
    System.out.println("hye..good morning");
+}
+  public void greetUser(){
+   System.out.println("hye..goodnight");
 }
 }
